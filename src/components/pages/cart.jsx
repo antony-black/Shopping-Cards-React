95276fb0a602360e442a9547100185c9d976cff6
Anton Knysh <anton@profairgames.com>
2024-03-31 16:53:19 +0300

    added the total section

--- src/components/pages/cart.jsx
+++ src/components/pages/cart.jsx
@@ -5,8 +5,18 @@ import { Link } from "react-router-dom";
 export default function Cart() {
   const { cart } = useSelector((state) => state);
   console.log(cart);
+  const getTotal = () => {
+    return cart.reduce((total, cur) => total + cur.price, 0).toFixed(2);
+  };
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center items-center">
+      <div className="border-2 border-red-400 rounded-2xl w-96 p-10 fixed top-24 right-10">
+        <h3 className="font-extrabold font-sans text-2xl text-gray-700 mb-3">
+          CONGRATULATIONS:
+        </h3>
+        <p className="text-red-700 font-semibold">{`You have choosen ${cart.length} items.`}</p>
+        <p className="text-red-700 font-semibold">{`Total: ${getTotal()}$.`}</p>
+      </div>
       {cart?.length ? (
         <>
           <div className="min-h-[80vh] grid md:grid-cols-2 max-w-6xl mx-auto">
